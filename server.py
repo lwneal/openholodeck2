5d9c0a13b34491c0491064fa100df1e8e8d9b21e
Lawrence Neal <nealla@lwneal.com>
2024-06-01 13:15:31 -0700

    Simpler server

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from quart import Quart, websocket, send_from_directory
 import asyncio
-from datetime import datetime
 import json
 import uuid
 
@@ -14,9 +13,28 @@ async def index():
 
 @app.websocket('/ws')
 async def ws():
-    # TODO
-    pass
+    client_id = str(uuid.uuid4())
+    clients[client_id] = websocket
+
+    await websocket.send(json.dumps({'type': 'client_id', 'id': client_id}))
+    await notify_clients()
+
+    try:
+        while True:
+            message = await websocket.receive()
+            data = json.loads(message)
+
+            target = data.get('target')
+            if target and target in clients:
+                await clients[target].send(message)
+    except:
+        del clients[client_id]
+        await notify_clients()
+
+async def notify_clients():
+    client_list = [{'id': client_id} for client_id in clients]
+    message = json.dumps({'type': 'clients', 'clients': client_list})
+    await asyncio.gather(*(client.send(message) for client in clients.values()))
 
 if __name__ == '__main__':
     app.run(port=8000)
-
