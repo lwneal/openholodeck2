9d2162829f5c21724bcac5a5df819ff6610687b6
Lawrence Neal <nealla@lwneal.com>
2024-06-01 09:49:04 -0700

    Add start script

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# If venv does not exist, create it
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the requirements
+pip install -r requirements.txt
+
+# Run the application
+python server.py
