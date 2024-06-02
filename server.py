853eafbeb1c4b9368ed4aa24a9ec08c44dc96110
Lawrence Neal <nealla@lwneal.com>
2024-06-01 17:52:10 -0700

    Back to old working version

--- server.py
+++ server.py
@@ -1,57 +1,29 @@
-from quart import Quart, websocket, send_from_directory
+from quart import Quart, websocket, jsonify, send_from_directory
 import asyncio
-import json
-import uuid
 
 app = Quart(__name__)
 
-clients = {}
+clients = set()
 
 @app.route('/')
 async def index():
     return await send_from_directory('static', 'client.html')
 
-@app.route('/rtc.js')
-async def rtc():
-    return await send_from_directory('static', 'rtc.js')
-
 @app.websocket('/ws')
 async def ws():
-    client_id = str(uuid.uuid4())
-    clients[client_id] = websocket
-
-    await websocket.send(json.dumps({'type': 'client_id', 'id': client_id}))
-    await notify_clients()
-
-    # Start a background task to send periodic pings
-    asyncio.create_task(periodic_ping(client_id))
-
+    global clients
+    clients.add(websocket._get_current_object())
+    print("Client connected")
     try:
         while True:
-            message = await websocket.receive()
-            data = json.loads(message)
-
-            target = data.get('target')
-            if target and target in clients:
-                await clients[target].send(message)
-    except:
-        del clients[client_id]
-        await notify_clients()
-
-async def notify_clients():
-    client_list = [{'id': client_id} for client_id in clients]
-    message = json.dumps({'type': 'clients', 'clients': client_list})
-    await asyncio.gather(*(client.send(message) for client in clients.values()))
-
-async def periodic_ping(client_id):
-    while client_id in clients:
-        try:
-            await clients[client_id].send(json.dumps({'type': 'ping', 'timestamp': str(uuid.uuid1())}))
-        except:
-            del clients[client_id]
-            await notify_clients()
-            break
-        await asyncio.sleep(2)
+            data = await websocket.receive()
+            for client in clients:
+                if client != websocket._get_current_object():
+                    await client.send(data)
+    except asyncio.CancelledError:
+        clients.remove(websocket._get_current_object())
+        print("Client disconnected")
+        raise
 
 if __name__ == '__main__':
     app.run(port=8000)
